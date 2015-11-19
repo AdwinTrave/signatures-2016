30867dea960f20a5900cf454a58f4c908b1c95d6
AdwinTrave <jxd3455@rit.edu>
2015-11-19 16:00:38 -0500

    Improved footer and header

--- elements/header.php
+++ elements/header.php
@@ -9,21 +9,18 @@ $displayThirdColumn = $blocks > 0 || $c->isEditMode();
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-sm-4 col-xs-6">
+            <div class="col-xs-12 text-center">
                 <?php
                 $a = new GlobalArea('Header Site Title');
                 $a->display();
                 ?>
             </div>
-            <div class="<?php if ($displayThirdColumn) { ?>col-sm-5 col-xs-6<?php } else { ?>col-sm-8 col-xs-6<?php } ?>">
+            <div class="col-sm-12 text-center">
                 <?php
                 $a = new GlobalArea('Header Navigation');
                 $a->display();
                 ?>
             </div>
-            <?php if ($displayThirdColumn) { ?>
-                <div class="col-sm-3 col-xs-12"><?php $as->display(); ?></div>
-            <?php } ?>
         </div>
     </div>
 </header>
